5841d11097ef522065f32cf32e0b0fb351580ba8
Evelyn perez uscanga <zs24016727@estudiantes.uv.mx>
2026-09-01 12:18:22 -0600

    agg

---
+++ Ejercicio4/Circulo.java
@@ -0,0 +1,9 @@
+public class Circulo extends FiguraGeometrica{
+    private double radio;
+    public Circulo(double radio){
+        thi.radio = radio;
+    }
+
+    public double getRadio() {
+        public double calcularArea()  }
+}
